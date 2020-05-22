4ff200c7a7c76989a8e6741c8bb91050fc0d9320
Александр <sasha.mirchev@gmail.com>
2020-05-22 01:43:36 -0700

    project commit

---
+++ build/precache-manifest.3b13c4387ffa51d8c12bdd12d91203fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20753b10e995dd2b61bb4265650c003c",
+    "url": "/github.io/react-pr-1/index.html"
+  },
+  {
+    "revision": "fefec26804d5e2a2fa49",
+    "url": "/github.io/react-pr-1/static/css/main.9a7605ab.chunk.css"
+  },
+  {
+    "revision": "0a81961a19a4651c2b9a",
+    "url": "/github.io/react-pr-1/static/js/2.5f7fc2e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-pr-1/static/js/2.5f7fc2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefec26804d5e2a2fa49",
+    "url": "/github.io/react-pr-1/static/js/main.25857995.chunk.js"
+  },
+  {
+    "revision": "3f4571ca5d19e38bc513",
+    "url": "/github.io/react-pr-1/static/js/runtime-main.768c85af.js"
+  },
+  {
+    "revision": "bac84d85a51077f84fdd9c82b0bbd58a",
+    "url": "/github.io/react-pr-1/static/media/homework_img_1.bac84d85.png"
+  }
+]);
